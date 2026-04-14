4a54f7d76127ce305dd98de07cfffce7550031ab
m-akiba <m-akiba@CD26301L.arksystems.co.jp>
2026-04-14 17:24:15 +0900

    Lesson3 challenge06まで終了しました。

---
+++ java100_questions_cac/src/main/java/jp/co/sss/java100_questions_cac/lesson04/challenge06/Advertisement.java
@@ -0,0 +1,17 @@
+package jp.co.sss.java100_questions_cac.lesson04.challenge06;
+
+public class Advertisement {
+	public static void main(String[] args) {
+		
+		System.out.println("私たちと一緒に働きませんか？");
+		System.out.println("\n【雇用条件】");
+		System.out.println("勤務時間：17:00～22:00");
+		System.out.println("時給：\\4,000");
+		System.out.println("待遇：");
+		System.out.println("・まかないあり");
+		System.out.println("・交通費支給");
+		System.out.println("・制服貸与");
+
+	}
+
+}
